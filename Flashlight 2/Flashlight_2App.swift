423a3bd87b92e7b213138ce077d43b54cbc49fa6
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-26 09:01:40 +0200

    Initial Commit

---
+++ Flashlight 2/Flashlight_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Flashlight_2App.swift
+//  Flashlight 2
+//
+//  Created by Sergey Lukaschuk on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Flashlight_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
